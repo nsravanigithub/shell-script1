3224ad7cd820a945cffe260df23f1846c7bafb60
nsravanigithub <nsravani1287@gmail.com>
2024-10-18 23:17:57 +0530

    some-message

---
+++ variables1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+user1=$1
+user2=$2
+echo "$user1:: How are you $user2"
+echo "$user2:: I am fine $user1.."
+echo "$user1:: Is everything fine with you"
+echo "$user2:: I am learning Devops $user1"
---
+++ variables2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Please enter username::"
+read -s USERNAME
+echo "Please enter your password::"
+read -s PASSWORD
+echo "username is: $USERNAME, Password is: $PASSWORD"
