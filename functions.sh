f4d0fb202e3c5b41c717ef5393c75852e72db1a5
nsravanigithub <nsravani1287@gmail.com>
2024-10-19 18:46:34 +0530

    some-message

---
+++ functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Using functions
+
+userid=$(id -u)
+Validate ()
+{
+    if ( $1 -eq 0 )
+    then
+    echo "Installation of $2 is:: Success"
+    else
+    echo "Installation of $2 is:: Failure"
+}
+if [ $userid -eq 0 ]
+then
+echo "User have root previlages"
+else
+echo "Run with root access"
+exit 1
+fi
+dnf install mysql -y
+Validate $? mysql
+
+dnf install nodejs -y
+Validate $? nodejs
