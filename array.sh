c5158f5b60e2fa49dd5b4ee2d2aad55a0683194d
nsravanigithub <nsravani1287@gmail.com>
2024-10-19 15:11:36 +0530

    some-message

--- array.sh
+++ array.sh
@@ -2,8 +2,8 @@
 
 #array code
 
-movies=${ "RRR" "Sagara sangamam" "Tholiprema" }
+movies=("RRR" "Sagara sangamam" "Tholiprema")
 
-echo "First movie is:: $movies[0]"
-echo "Second movie is:: $movies[1]"
-echo "Third movie is:: $movies[2]"
+echo "First movie is:: ${movies[0]}"
+echo "Second movie is:: ${movies[1]}"
+echo "Third movie is:: ${movies[2]}"
