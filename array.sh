7528688e5938eeb3fe4f80aed1c346bb1fc24934
nsravanigithub <nsravani1287@gmail.com>
2024-10-19 15:07:02 +0530

    some-message

---
+++ array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#array code
+
+movies=["RRR" "Sagara sangamam" "Tholiprema"]
+
+echo "First movie is:: $movies[0]"
+echo "Second movie is:: $movies[1]"
+echo "Third movie is:: $movies[2]"
