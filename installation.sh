96cc893af3e39a34c16df68220c83ad332d25f67
nsravanigithub <nsravani1287@gmail.com>
2024-10-19 17:00:48 +0530

    some-message

--- installation.sh
+++ installation.sh
@@ -18,3 +18,12 @@ else
 echo "Installation not successfull"
 exit 1
 fi
+dnf install nodejjs -y
+
+if [ $? -eq 0 ]
+then
+echo "Installation successful"
+else
+echo "Installation not successfull"
+exit 1
+fi
